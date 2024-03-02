c53dbde0784e36776ffd700c9e63e559d7f681e4
Arnab Das <mazumderarnabdas2003@gmail.com>
2024-03-02 11:51:00 +0530

    update admin login

--- collage_fest/fest_app/models.py
+++ collage_fest/fest_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class student_basic(models.Model):
+    name=models.CharField(max_length=50)
+    roll=models.IntegerField()
+    payment_status=models.IntegerField()
+    class Meta:
+        db_table='student_basic'
