f25736ada95c66947c1546eedd5a6a80062584e4
Arnab Das <mazumderarnabdas2003@gmail.com>
2024-03-06 10:49:41 +0530

    update admin login

--- collage_fest/fest_app/urls.py
+++ collage_fest/fest_app/urls.py
@@ -4,4 +4,5 @@ urlpatterns=[
     path("",views.login),
     path('home',views.home),
     path('student_login',views.student_login),
+    path('s_login',views.s_login),
 ]
--- collage_fest/fest_app/views.py
+++ collage_fest/fest_app/views.py
@@ -10,3 +10,10 @@ def home(request):
     return render(request,'index.html')
 def student_login(request):
     return render(request,'stu_login.html')
+def s_login(request):
+    u=student_basic()
+    u.name=request.GET['a1']
+    u.roll=request.GET['a2']
+    u.payment_status=0
+    u.save()
+    return render(request,'stu_login.html')
