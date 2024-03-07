9e151855e34f3cab15b848c1ddbb68e48120ab84
Arnab Das <mazumderarnabdas2003@gmail.com>
2024-03-07 21:44:31 +0530

    change database by Arnab

--- collage_fest/fest_app/models.py
+++ collage_fest/fest_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class student_basic(models.Model):
     name=models.CharField(max_length=50)
     roll=models.IntegerField()
+    collage_status=models.IntegerField()
+    college_name=models.CharField(max_length=300)
     payment_status=models.IntegerField()
+
     class Meta:
         db_table='student_basic'
