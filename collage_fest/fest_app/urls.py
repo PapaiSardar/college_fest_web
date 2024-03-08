6ee6dde7c91aae54efed364f9fe54842fab6496a
Mrinmoy <mrinmoy70034@gmail.com>
2024-03-08 10:25:48 +0530

    Event update by Mrinmoy

--- collage_fest/fest_app/urls.py
+++ collage_fest/fest_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path("",views.home),
+    path("home",views.home),
     path('student_login',views.student_login),
     path('s_login',views.s_login),
     path('events',views.events),
