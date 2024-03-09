00eb3e156589e1313448a2bb8690512e6c816a9a
Arnab Das <mazumderarnabdas2003@gmail.com>
2024-03-10 00:01:21 +0530

    make check payment  by Arnab

--- collage_fest/fest_app/views.py
+++ collage_fest/fest_app/views.py
@@ -31,3 +31,33 @@ def s_login(request):
     return render(request,'thank_reg.html')
 def login_stu(request):
     return render(request,'login.html')
+def p_status(request):
+    return render(request,'payment_check.html')
+def pay_check(request):
+    a=request.GET['a1']
+    if a in student_detalis.objects.filter(roll=a):
+        u=student_detalis.objects.get(roll=a)
+        if u.payment_status==1:
+            return render(request,'payment_check.html',{'c':'payment clear'})
+        elif u.payment_status==0:
+            return render(request,'payment_check.html',{'c':'Payment not clear if you already pay plese please call 7470255315'})
+        else:
+            return render(request,'payment_check.html',{'c':'data not found reg. please'})
+    else:
+        return render(request,'stu_login.html')
+def check_payment(request):
+    if request.method == 'POST':
+        roll = request.POST.get('roll')  # Assuming roll is submitted via POST
+        
+        try:
+            student = student_detalis.objects.get(roll=roll)
+            if student.payment_status == 1:
+                payment_status = "Payment is completed."
+            else:
+                payment_status = "Payment is not completed.If your payment is already done please call 7470255315."
+        except student_details.DoesNotExist:
+            payment_status = "Student with this roll number does not exist."
+
+        return render(request, 'payment_status.html', {'payment_status': payment_status})
+    else:
+        return render(request, 'payment_status.html')
